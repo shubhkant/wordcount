328f7ac3499969bacb41d9554d5a565d14afbb97
shubhkant <46314274+shubhkant@users.noreply.github.com>
2019-01-02 14:35:58 +0530

    Update urls.py

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.homepage , name = 'home'),
+    path('',views.home , name = 'home'),
 	
 	path ('count/' ,views.count , name='count'),
 ]
